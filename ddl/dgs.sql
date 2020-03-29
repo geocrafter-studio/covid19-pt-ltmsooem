72e7d50cfb79297829aefdb53d752923bb723c96
Fernando Ribeiro <fernandinand@gmail.com>
2020-03-29 13:17:29 +0100

    Updates for 29032020. Add 48h progress to municipal and regional stats. Minor fixes.

--- ddl/dgs.sql
+++ ddl/dgs.sql
@@ -1,71 +1,78 @@
-CREATE SCHEMA dgs;
+create schema dgs;
 
 -- daily municipal data placeholder
-CREATE TABLE dgs.daily_mun (
-	id serial NOT NULL,
-	objectid int4 NOT NULL,
-	cases int4 NOT NULL DEFAULT 0,
-	"date" timestamp NOT NULL,
-	CONSTRAINT pk_daily_mun PRIMARY KEY (id)
+create table dgs.daily_mun (
+	id serial not null,
+	objectid int4 not null,
+	cases int4 not null default 0,
+	"date" timestamp not null,
+	constraint pk_daily_mun primary key (id)
 );
 
 -- municipal views
-CREATE OR REPLACE VIEW dgs.v_daily_mun
-AS SELECT row_number() over(order by b.date) as id,
+create or replace view dgs.v_daily_mun
+as select row_number() over(order by b.date) as id,
     b.objectid,
     a.concelho,
     b.cases,
     b.cases - (lag(b.cases, 1) over (
-   				PARTITION BY a.concelho
-   				ORDER BY a.concelho, b."date")) as cases_progress,
+   				partition by a.concelho
+   				order by a.concelho, b."date")) as cases_progress,
     b.date,
     a.geom
-   FROM geo.pt_mun a
-     JOIN dgs.daily_mun b ON a.objectid = b.objectid
-   ORDER BY 3,6;
+   from geo.pt_mun a
+     join dgs.daily_mun b on a.objectid = b.objectid
+   order by 3,6;
 
-CREATE OR REPLACE VIEW dgs.v_daily_mun_last
-AS SELECT b.objectid,
+create materialized view dgs.mv_daily_mun_last
+as select b.objectid,
    a.concelho,
    b.cases,
    z.cases_progress,
    a.geom
-  FROM geo.pt_mun a
-    JOIN dgs.daily_mun b ON a.objectid = b.objectid
+  from geo.pt_mun a
+    join dgs.daily_mun b on a.objectid = b.objectid
     join lateral (
     	select vdm."date", vdm.cases_progress from dgs.v_daily_mun vdm where vdm.concelho = a.concelho
     ) z on b."date" = z."date"
   where b.date > now() - interval '1d'
   order by 2;
 
--- regional MV placeholder
-CREATE MATERIALIZED VIEW dgs.region_stats AS
-  SELECT b.objectid,
+-- regional mv placeholder
+create materialized view dgs.region_stats as
+  select b.objectid,
     a.datarel,
-    min(a.dist_casosconf) AS confirmed,
-    min(a.dist_obitos) AS deaths,
-    min(a.dist_recuperados) AS recovered
-   FROM esri.casos_regiao a
-     JOIN geo.pt_regions b ON st_intersects(st_buffer(st_transform(a.geom, 3763), 10000::double precision), st_makevalid(b.geom))
-  group by b.objectid, a.datarel;
+    min(a.dist_casosconf) as confirmed,
+    min(a.dist_obitos) as deaths,
+    min(a.dist_recuperados) as recovered
+   from esri.casos_regiao a
+     join geo.pt_regions b on st_intersects(st_buffer(st_transform(a.geom, 3763), 10000::double precision), st_makevalid(b.geom))
+  group by 1, 2;
 
-CREATE OR REPLACE VIEW dgs.v_daily_regions
-AS SELECT row_number() over(order by a.datarel) as id,
+-- daily export view
+create or replace view dgs.v_region_stats_export as
+	select * from dgs.region_stats where datarel > now() - interval '1d';
+
+create or replace view dgs.v_daily_regions
+as select row_number() over(order by a.datarel) as id,
     b.name,
     a.datarel,
     a.confirmed,
     a.deaths,
     a.recovered,
     b.geom
-   FROM dgs.region_stats a
-     JOIN geo.pt_regions b ON a.objectid = b.objectid;
+   from dgs.region_stats a
+     join geo.pt_regions b on a.objectid = b.objectid;
 
-CREATE OR REPLACE VIEW dgs.v_daily_regions_last
-AS SELECT b.name,
-   a.confirmed,
-   a.deaths,
-   a.recovered,
-   b.geom
-  FROM dgs.region_stats a
-    JOIN geo.pt_regions b ON a.objectid = b.objectid
-  where a.datarel > now() - interval '1d';
+create or replace view dgs.v_daily_regions_last
+as select b.name,
+  a.confirmed,
+  a.confirmed_progress,
+  a.deaths,
+  a.deaths_progress,
+  a.recovered,
+  a.recovered_progress,
+  b.geom
+ from dgs.v_daily_regions a
+   join geo.pt_regions b on a."name" like b."name"
+ where a.datarel > now() - interval '1d';
